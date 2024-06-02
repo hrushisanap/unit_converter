ca3a6360b2d026eab73ccc8195e3c67bab5813a3
Hrushi Sanap <hrushinstudy@gmail.com>
2024-06-02 15:30:22 +0100

    distance converter added
    distance converter added to existing code in mass_converter branch

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 import unit_converter_class as ucc
 
 
-print("""Choose from following units:
+def main():
+    temp_var = input(("""What do you want to convert?\nDistance | Mass | Speed\n""")).title()
+    
+    if temp_var == "Weight" or temp_var == "Mass":
+        print(mass_converter())
+        
+    if temp_var == "Distance":
+        print(distance_converter())
+        
+    if temp_var == "Velocity" or temp_var == "Speed":
+        speed_converter()
+        
+
+def mass_converter():
+    print("""Choose from following units:
     Picogram | Nanogram | Microgram | Milligram | Centigram | Decigram |
     Gram | Dekagram | Hectogram | Kilogram | Tonne | Kilotonne |
     Megatonne | Gigatonne | Pound | Ounce | Stone | US Ton \n""")
 
-mass_in_unit = input("you want to convert from: ").title()
-mass_in_value = float(input(f"value of {mass_in_unit}s: "))
-mass_out_unit = input("you want to convert to: ").title()
+    mass_in_unit = input("you want to convert from: ").title()
+    mass_in_value = float(input(f"value of {mass_in_unit}s: "))
+    mass_out_unit = input("you want to convert to: ").title()
 
-x = ucc.mass_unit_converter(mass_in_unit, mass_in_value, mass_out_unit)
-print(x)
+    return ucc.mass_unit_converter(mass_in_unit, mass_in_value, mass_out_unit)
+    
+    
+def distance_converter():
+    print("""Choose from following units:
+     "Picometer | Nanometer | Micrometer | Millimeter | Centimeter | 
+     Decimeter | Meter | Dekameter | Hectometer | Kilometer | Mile | 
+     Inch | Yard | Foot | Rod\n""")
+
+    distance_in_unit = input("you want to convert from: ").title()
+    distance_in_value = float(input(f"value of {distance_in_unit}s: "))
+    distance_out_unit = input("you want to convert to: ").title()
+
+    return ucc.distance_unit_converter(distance_in_unit, distance_in_value, distance_out_unit)
+
+
+def speed_converter():
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- unit_converter_class.py
+++ unit_converter_class.py
@@ -19,6 +19,24 @@ dict_units2grams_unit = {"Picogram": 0.000000000001,
                     "US Ton": 907185
                     }
 
+dict_units2meters_unit = {"Picometer": 0.000000000001,
+                    "Nanometer": 0.000000001,
+                    "Micrometer": 0.000001,
+                    "Millimeter": 0.001,
+                    "Centimeter": 0.01,
+                    "Decimeter": 0.1,
+                    "Meter": 1,
+                    "Dekameter": 10,
+                    "Hectometer": 100,
+                    "Kilometer": 1000,
+                    "Mile":1609.34,
+                    "Inch": 0.0254,
+                    "Yard": 0.9144, 
+                    "Foot": 0.3048,
+                    "Rod": 5.0292
+                    }
+
+
 
 def in2grams(in_unit, in_value):
     return in_value * dict_units2grams_unit[in_unit]
@@ -26,11 +44,21 @@ def in2grams(in_unit, in_value):
 def grams2out(gms_value, out_unit):
     return gms_value / dict_units2grams_unit[out_unit]
 
-
 def mass_unit_converter(in_unit, in_value, out_unit):
     in_grams = in2grams(in_unit, in_value)
     return round(grams2out(in_grams, out_unit), 3)
 
+
+
+def in2meters(in_unit, in_value):
+    return in_value * dict_units2meters_unit[in_unit]
+
+def meters2out(gms_value, out_unit):
+    return gms_value / dict_units2meters_unit[out_unit]
+
+def distance_unit_converter(in_unit, in_value, out_unit):
+    in_grams = in2meters(in_unit, in_value)
+    return round(meters2out(in_grams, out_unit), 3)
 
 
     
